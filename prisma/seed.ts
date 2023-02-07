1a60d8c0ac2ce1aa4818a8d681284225746e4a4e
Vitali177 <vskrabatun@gmail.com>
2023-02-08 00:42:10 +0300

    create seed file for filling data in the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,5 +8,78 @@ type Author = {
 type Book = {
   title: string;
   isFiction: boolean;
-  datePublish: Date;
+  datePublished: Date;
 };
+
+async function seed() {
+  await Promise.all(
+    getAuthors().map((author) => {
+      return db.author.create({
+        data: {
+          firstName: author.firstName,
+          lastName: author.lastName
+        }
+      })
+    })
+  );
+
+  const author = await db.author.findFirst({
+    where: {
+      firstName: 'Yuval Noah',
+    },
+  });
+
+  await Promise.all(
+    getBooks().map((book) => {
+      if (!author) return;
+      const { title, isFiction, datePublished } = book;
+      return db.book.create({
+        data: {
+          title,
+          isFiction,
+          datePublished,
+          authorId: author.id,
+        },
+      })
+    })
+  );
+}
+
+seed();
+
+function getAuthors(): Array<Author> {
+  return [
+    {
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      firstName: 'William',
+      lastName: 'Shakespeare'
+    },
+    {
+      firstName: 'Yuval Noah',
+      lastName: 'Harari'
+    },
+  ];
+}
+
+function getBooks(): Array<Book> {
+  return [
+    {
+      title: 'Sapiens',
+      isFiction: false,
+      datePublished: new Date(),
+    },
+    {
+      title: 'Homo Deus',
+      isFiction: false,
+      datePublished: new Date(),
+    },
+    {
+      title: 'The Ugly Duckling',
+      isFiction: true,
+      datePublished: new Date(),
+    },
+  ];
+}
