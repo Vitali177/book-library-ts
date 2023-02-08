42dbeb31507a6df2a5977bc1899114c426d2b6db
Vitali177 <vskrabatun@gmail.com>
2023-02-08 09:50:00 +0300

    add function to create Book

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -7,8 +7,14 @@ type BookRead = {
   datePublished: Date;
   isFiction: boolean;
   author: Author;
-  // authorId: number;
-}
+};
+
+type BookWrite = {
+  title: string;
+  datePublished: Date;
+  isFiction: boolean;
+  authorId: number;
+};
 
 export const listBooks = async (): Promise<BookRead[]> => {
   return db.book.findMany({
@@ -32,6 +38,33 @@ export const getBook = async (id: number): Promise<BookRead | null> => {
   return db.book.findUnique({
     where: {
       id
+    },
+    select: {
+      id: true,
+      title: true,
+      datePublished: true,
+      isFiction: true,
+      author: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true
+        }
+      }
+    }
+  });
+};
+
+export const createBook = async (book: BookWrite): Promise<BookRead> => {
+  const { title, authorId, datePublished, isFiction } = book;
+  const parsedDate: Date = new Date(datePublished);
+
+  return db.book.create({
+    data: {
+      title,
+      authorId,
+      datePublished: parsedDate,
+      isFiction
     },
     select: {
       id: true,
